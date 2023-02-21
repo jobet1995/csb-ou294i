f7c6e11ce1951e64d5f1a60b98e557e353cd1f42
Code Sandbox <hello@codesandbox.io>
2023-02-21 10:54:39 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.576e1bf022bae8cb00654fc835c3492d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62a5f702e8482299b9df44cf786354b",
+    "url": "https://jobet1995.github.io/csb-ou294i/index.html"
+  },
+  {
+    "revision": "645d1e497e055b517c41",
+    "url": "https://jobet1995.github.io/csb-ou294i/static/js/2.a4616265.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://jobet1995.github.io/csb-ou294i/static/js/2.a4616265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a67406803b757c087d",
+    "url": "https://jobet1995.github.io/csb-ou294i/static/js/main.bbedb53e.chunk.js"
+  },
+  {
+    "revision": "3a876c67e6b42d40e950",
+    "url": "https://jobet1995.github.io/csb-ou294i/static/js/runtime-main.01ceec16.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://jobet1995.github.io/csb-ou294i/precache-manifest.576e1bf022bae8cb00654fc835c3492d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://jobet1995.github.io/csb-ou294i/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
